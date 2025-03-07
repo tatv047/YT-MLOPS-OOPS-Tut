d541a4d3d5574de03016865ec2d13cca1cab65e1
tatv047 <91747675+tatv047@users.noreply.github.com>
2025-03-07 11:26:41 +0530

    encapuslation,getter-setter and static method done

--- oops_proj.py
+++ oops_proj.py
@@ -3,11 +3,34 @@ Dummy social media project for understanding classes and objects.
 """
 
 class chatbook:
+
+    __user_id = 1 # static variable and can only be accessed by the class not self.
+
     def __init__(self):
+        self.id = chatbook.__user_id
+        chatbook.__user_id += 1
+        self.__name = "Default User" # a hidden attribute now - encapsulation
+        # self.user_id = 0
+        # self.user_id+= 1
         self.username = ''
         self.password = ''
         self.loggedin = False
-        self.menu()
+        # self.menu()
+
+
+    @staticmethod # you don't need self for static method because it's not the object accessing it, hence it's also diff this way 
+    def get_id():
+        return chatbook.__user_id
+    
+    @staticmethod
+    def set_id(value):
+        chatbook.__user_id = value 
+
+    def get_name(self):
+        return self.__name
+    
+    def set_name(self,value):
+        self.__name = value
     
     def menu(self):
         user_input = input("""Welcome to ChatBook !! How would you like to proceed?
--- rough.py
+++ rough.py
@@ -1,12 +1,17 @@
-# lst = [1,2,3]
-# myStr = "MLops is love"
-# myInt = 155
-
-# # print(type(myStr))
-
-# a = 'x'
-# b = 'y'
-# print(a+b)
-
 from oops_proj import chatbook
 user1 = chatbook()
+print(user1.id)
+
+
+"Using Static Method directly  from class rather than object"
+# chatbook.set_id(10)
+# user2 = chatbook()
+# print(user2.id)
+
+# user3 = chatbook()
+# print(user3.id)
+
+"Gettter and setter"
+# print(user1.get_name())
+# user1.set_name("Samir")
+# print(user1.get_name())
